0150b9336720ec8042db34dd35e9737cb2c02ff2
Sebastián Collado Montañez <scollado@ujaen.es>
2018-11-22 13:06:45 +0100

    init

---
+++ webserver.js
@@ -0,0 +1,48 @@
+var http = require('http').createServer(handler); //require http server, and create server with function handler()
+var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+
+http.listen(8080); //listen to port 8080
+
+function handler(req, res) { //create server
+  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
+    if (err) {
+      res.writeHead(404, {
+        'Content-Type': 'text/html'
+      }); //display 404 on error
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    }); //write HTML
+    res.write(data); //write data from index.html
+    return res.end();
+  });
+}
+
+/** WebSocket Server **/
+var messages = [];
+
+// WebSocket conection
+io.sockets.on('connection', function(socket) {
+
+  // On new conection, send all messages
+  socket.emit("init", messages);
+
+  // Input methods
+  socket.on('newMessage', function(message) {
+    var aux = message;
+    aux.date = new Date();
+
+    /*var aux = {
+      message: message,
+      date: new Date()
+    };*/
+
+    messages.push(aux);
+    console.log(messages);
+
+    socket.broadcast.emit("init", messages);
+    socket.emit("init", messages);
+  });
+});
